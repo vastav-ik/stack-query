ec2ddfd3945e233094a1470a5beb4a9c28c5be2a
vastav-ik <guptavastav9389@gmail.com>
2025-12-10 11:32:50 +0530

    Add "use client" directive to layout and login page components

--- src/(auth)/layout.tsx
+++ src/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useAuthStore } from "@/store/Auth";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
--- src/(auth)/login/page.tsx
+++ src/(auth)/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useAuthStore } from "@/store/Auth";
 import React, { useState } from "react";
 
